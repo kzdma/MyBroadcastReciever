679713c4c76129ff8338ea5dccf367af2975c52a
Kostya <1002862406@hityash.org.il>
2025-09-25 09:25:04 +0300

    Headset Broadcast Reciever

---
+++ BroadCast/BroadCastHeadSets.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyBroadcastReciever.BroadCast
+{
+    [BroadcastReceiver]
+    public class BroadCastHeadSets : BroadcastReceiver
+    {
+        TextView tv;
+
+        public BroadCastHeadSets() { }
+        public BroadCastHeadSets(TextView tv) { this.tv = tv; }
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            //Toast.MakeText(context, "Received intent!", ToastLength.Short).Show();
+            //Log.Debug(TAG, "HEADSET RECEIVER GOT MESSAGE");
+            if (intent.Action == Intent.ActionHeadsetPlug)
+            {
+                int state = intent.GetIntExtra("state", -1);
+                int microphone = intent.GetIntExtra("microphone", 0);
+                switch (state)
+                {
+                    case 0:
+                        tv.Text = "headset unplugged";
+                        Toast.MakeText(context, "headset unplugged", ToastLength.Short).Show();
+                        break;
+                    case 1:
+                        tv.Text = "headset plugged, with microphone + " + (microphone == 1);
+                        Toast.MakeText(context, tv.Text, ToastLength.Short).Show();
+                        break;
+                    default:
+                        tv.Text = "unknown state ";
+                        Toast.MakeText(context, "unknown state ", ToastLength.Short).Show();
+                        break;
+                }
+            }
+        }
+    }
+}
--- MainActivity.cs
+++ MainActivity.cs
@@ -1,20 +1,43 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
+using MyBroadcastReciever.BroadCast;
+using Android.Content;
 
 namespace MyBroadcastReciever
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        TextView tv;
+        BroadCastHeadSets brHeadSet;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            tv = FindViewById<TextView>(Resource.Id.tvHeadsetState);
+            brHeadSet = new BroadCastHeadSets(tv);
         }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            RegisterReceiver(brHeadSet, 
+                new Android.Content.IntentFilter(Intent.ActionHeadsetPlug));
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            UnregisterReceiver(brHeadSet);
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
