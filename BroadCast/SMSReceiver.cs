92b174176e731c0ec129362e399a6560ba173030
Kostya <1002862406@hityash.org.il>
2025-09-26 12:12:21 +0300

    SMS Broadcast Reciever

---
+++ BroadCast/SMSReceiver.cs
@@ -0,0 +1,77 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Provider;
+using Android.Runtime;
+using Android.Telephony;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyBroadcastReciever.BroadCast
+{
+    [BroadcastReceiver]
+    public class SMSReceiver : BroadcastReceiver
+    {
+        private TextView tvSMS;
+        private const string TAG = "MYAPP";
+
+        public SMSReceiver() { }
+        public SMSReceiver(TextView tv) { this.tvSMS = tv; }
+        public override void OnReceive(Context context, Intent intent)
+        {
+            Log.Debug(TAG, "New SMS Arrived");
+            GetSMSAction(context, intent);
+        }
+
+        private void GetSMSAction(Context context, Intent intent)
+        {
+            string strMessage = "", sender = "";
+            SmsMessage[] msgs;                    //using Android.Telephony;
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Kitkat)
+            {
+                msgs = Telephony.Sms.Intents.GetMessagesFromIntent(intent); //smsMessage = msgs[0];
+                foreach (SmsMessage msg in msgs)
+                {
+                    sender = msg.OriginatingAddress;
+                    strMessage += "SMS From: " + msg.OriginatingAddress;
+                    strMessage += "\n";
+                    strMessage += msg.MessageBody;
+                    strMessage += "\n";
+                }
+            }
+            else //Old SDK versions
+            {
+                if (intent.HasExtra("pdus"))
+                {
+                    var smsArray = (Java.Lang.Object[])intent.Extras.Get("pdus");
+                    foreach (var item in smsArray)
+                    {
+                        var sms = SmsMessage.CreateFromPdu((byte[])item);
+                        sender = sms.OriginatingAddress;
+                        strMessage += "SMS From: " + sms.OriginatingAddress;
+                        strMessage += "\n";
+                        strMessage += sms.MessageBody;
+                        strMessage += "\n";
+                    }
+                }
+            }
+            
+            tvSMS.Text = strMessage; //Geting an exception
+
+            Log.Debug(TAG, $"OriginatingAddress: {sender}");
+
+            if (strMessage.Length > 0)
+            {
+                Intent smsIntent = new Intent(context, typeof(MainActivity));
+                smsIntent.PutExtra("info", strMessage);
+                smsIntent.AddFlags(ActivityFlags.NewTask);
+                context.StartActivity(smsIntent);
+            }           
+        }
+    }
+}
--- MainActivity.cs
+++ MainActivity.cs
@@ -1,11 +1,14 @@
 ﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Nfc;
 using Android.OS;
 using Android.Runtime;
+using Android.Util;
 using Android.Widget;
 using AndroidX.AppCompat.App;
 using MyBroadcastReciever.BroadCast;
-using Android.Content;
-using Android.Util;
+using System;
 
 namespace MyBroadcastReciever
 {
@@ -14,6 +17,14 @@ namespace MyBroadcastReciever
     {
         TextView tv;
         BroadCastHeadSets brHeadSet;
+        SMSReceiver sms_receiver;
+        Button btnRegister, btnUnregister;
+        TextView tvSMS;
+
+        Android.Content.ISharedPreferences mySHP;
+        bool IsRegistered;
+
+        private const string TAG = "MYAPP";
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -22,10 +33,92 @@ namespace MyBroadcastReciever
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
+            //Checking if SMS Permissions Granted
+            if ((ApplicationContext.CheckCallingOrSelfPermission(Android.Manifest.Permission.ReceiveSms) !=
+                                                                                Android.Content.PM.Permission.Granted))
+            {
+                RequestPermissions(new string[] { Android.Manifest.Permission.ReceiveSms }, 1);
+            }
+            MyInitilize();
+            Log.Debug(TAG, "OnCreate: IsRegistered " + IsRegistered.ToString());           
+        }
+
+        private void MyInitilize()
+        {
             tv = FindViewById<TextView>(Resource.Id.tvHeadsetState);
             brHeadSet = new BroadCastHeadSets(tv);
 
-            Log.Debug("MYAPP", "MainActivity OnCreate");
+            mySHP = this.GetSharedPreferences("ReceiverStatus", Android.Content.FileCreationMode.Private);
+            IsRegistered = mySHP.GetBoolean("ReceiverStatus", false);
+            btnRegister = FindViewById<Button>(Resource.Id.btnRegister);
+            btnUnregister = FindViewById<Button>(Resource.Id.btnUnRegister);
+
+            tv = FindViewById<TextView>(Resource.Id.tvInSMSText);
+            sms_receiver = new SMSReceiver(tv);
+
+            btnRegister.Click += BtnRegister_Click;
+            btnUnregister.Click += BtnUnregister_Click;     
+            if (IsRegistered)
+            {
+                ButtonDisable(btnRegister);
+                ButtonEnable(btnUnregister);
+            }
+            else
+            {
+                ButtonDisable(btnUnregister);
+                ButtonEnable(btnRegister);
+            }
+            //Showing the SMS body message
+            if (Intent.HasExtra("info"))
+            {
+                string info = Intent.GetStringExtra("info");
+                tv.Text = info;
+            }
+        }
+
+        private void ButtonEnable(Button btn)
+        {
+            btn.SetTextColor(Color.Black);
+            btn.SetBackgroundColor(Color.ParseColor("#b3b3b3"));
+            btn.Enabled = true;
+        }
+
+        private void ButtonDisable(Button btn)
+        {
+            btn.SetTextColor(Color.ParseColor("#ffffff"));
+            btn.SetBackgroundColor(Color.ParseColor("#cccccc"));
+            btn.Enabled = false;
+        }
+
+        private void BtnUnregister_Click(object sender, EventArgs e)
+        {
+            if (IsRegistered)
+            {
+                IsRegistered = false;
+                var editor = mySHP.Edit();
+                editor.PutBoolean("ReceiverStatus", IsRegistered);
+                editor.Commit();
+                UnregisterReceiver(sms_receiver);
+                ButtonEnable(btnRegister);
+                ButtonDisable(btnUnregister);
+                Log.Debug(TAG, "BTN UnRegister: IsRegistered " + IsRegistered.ToString());
+            }
+        }
+
+        private void BtnRegister_Click(object sender, EventArgs e)
+        {            
+            if (!IsRegistered)
+            {
+                IsRegistered = true;
+                var editor = mySHP.Edit();
+                editor.PutBoolean("ReceiverStatus", IsRegistered);
+                editor.Commit();
+                ButtonEnable(btnUnregister);
+                ButtonDisable(btnRegister);
+                RegisterReceiver(sms_receiver, new
+                IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
+                Log.Debug(TAG, "BTN Registered: IsRegistered " + IsRegistered.ToString());
+            }
         }
 
         protected override void OnResume()
@@ -33,12 +126,37 @@ namespace MyBroadcastReciever
             base.OnResume();
             RegisterReceiver(brHeadSet, 
                 new Android.Content.IntentFilter(Intent.ActionHeadsetPlug));
+
+            if (IsRegistered)
+            {
+                RegisterReceiver(sms_receiver, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
+                Log.Debug(TAG, "OnResume Registered: IsRegistered " + IsRegistered.ToString());
+            }
         }
 
         protected override void OnPause()
         {
             base.OnPause();
             UnregisterReceiver(brHeadSet);
+
+            if (IsRegistered)
+            {
+                UnregisterReceiver(sms_receiver);
+                Log.Debug(TAG, "OnPause Unregistered: IsRegistered " + IsRegistered.ToString());
+            }
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            if (IsRegistered)
+            {
+                IsRegistered = false;
+                var editor = mySHP.Edit();
+                editor.PutBoolean("ReceiverStatus", IsRegistered);
+                editor.Commit();
+                UnregisterReceiver(sms_receiver);
+            }
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
@@ -46,6 +164,18 @@ namespace MyBroadcastReciever
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            if (requestCode == 1)
+            {
+                if (grantResults[0] != Android.Content.PM.Permission.Granted)
+                {
+                    //Toast.MakeText(this, "Oh No!!! Permission Denied. App will EXIT", ToastLength.Long).Show();
+                    //Two examples to exit from App
+                    //System.Environment.Exit(0);
+                    //Android.OS.Process.KillProcess(Android.OS.Process.MyPid());
+                    tv.Text = "SMS SEND/RECEIVE Permission denied, application may not work correctly!";
+                }
+            }
         }
     }
 }
